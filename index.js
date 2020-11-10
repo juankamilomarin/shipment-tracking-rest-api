c6d8e09eea0bd059cd125ff3cc9ee7e520449296
Juan Camilo Marin <juankamilomarin@gmail.com>
2020-11-09 20:45:24 -0500

    Configure simple node-postgres client

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const config = require('./src/common/config/env.config.js');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { dbClient } = require('./src/common/services/node-postgres.service.js');
 
-app.get('/', (req, res) => {
-  res.send('This is shimpent tracking REST Api')
+app.get('/', async (req, res) => {
+    const text = "SELECT id, name, active FROM store WHERE name ilike $1";
+    const values = ['%amazon%'];
+
+    dbClient.query(text, values)
+        .then(result => res.send(result.rows[0]))
+        .catch(error => {
+            console.error(error.stack)
+            res.status(500).send(error.message)
+        })
+        .then(() => dbClient.end())
 })
 
 app.listen(config.port, () => {
-  console.log(`Application listening at port ${config.port}`)
+    console.log(`Application listening at port ${config.port}`)
 })
--- src/common/config/env.config.js
+++ src/common/config/env.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    "port": 3010
+    port: 3010,
+    postgres: {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'postgres',
+        port: 5432
+    }
 };
