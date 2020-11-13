8265229538221b2ccc5825094f3c31eddc14598b
Juan Camilo Marin <juankamilomarin@gmail.com>
2020-11-13 15:25:17 -0500

    Simplify stores controller

--- src/stores/routes.config.js
+++ src/stores/routes.config.js
@@ -1,20 +1,8 @@
 const StoresController = require('./stores.controller')
 
 exports.routesConfig = function (app) {
-    app.get('/stores', [
-        StoresController.list
-    ]);
-
-    app.get('/stores/:storeId', [
-        StoresController.getById
-    ]);
-
-    app.post('/stores', [
-        StoresController.insert
-    ]);
-
-    app.delete('/stores/:storeId', [
-        StoresController.delete
-    ]);
-
+    app.get('/stores', StoresController.list);
+    app.get('/stores/:storeId', StoresController.getById);
+    app.post('/stores', StoresController.insert);
+    app.delete('/stores/:storeId', StoresController.delete);
 };
